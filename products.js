adefe2f4fc4e8985e495b9c64ddadae94279c64c
UzmaAkhtarShivalli <uzzuakhtar@gmail.com>
2023-06-10 15:45:52 +0530

    backend added

---
+++ products.js
@@ -0,0 +1,9 @@
+const productsList=[
+    {id:1,name:"uzma",age:20,gender:"female"},
+    {id:2,name:"uzma",age:25,gender:"female"},
+    {id:3,name:"uzma",age:20,gender:"female"},
+    {id:4,name:"uzma",age:20,gender:"female"},
+    {id:5,name:"uzma",age:20,gender:"female"}
+]
+module.exports=productsList
+
